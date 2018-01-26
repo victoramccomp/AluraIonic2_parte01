b2c88b43979be9f599e49bdfbfaca36cf81ccdb6
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2018-01-26 11:07:28 -0200

    Aula 02 - Consumindo a API do cliente
    Atenção ao app.module.ts. Está diferente do vídeo e precisei importar o HttpModule nele

--- aluraCar/src/app/app.module.ts
+++ aluraCar/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,8 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- aluraCar/src/pages/home/home.ts
+++ aluraCar/src/pages/home/home.ts
@@ -1,33 +1,48 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
 	selector: 'page-home',
 	templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
 	public carros;
 
-	constructor(public navCtrl: NavController) {
-		this.carros = [
-			{ nome: 'Creta', preco: 50000 },
-			{ nome: 'HB20', preco: 25000 },
-			{ nome: 'HB20S', preco: 60000 },
-			{ nome: 'HB20X', preco: 40000 },
-			{ nome: 'ix35', preco: 27000 },
-			{ nome: 'Caravan', preco: 76000 },
-			{ nome: 'Celta', preco: 100000 },
-			{ nome: 'Chevette Hatch', preco: 55000 },
-			{ nome: 'Chevette', preco: 30000 },
-			{ nome: 'Chevy', preco: 60000 },
-			{ nome: 'Cobalt', preco: 30000 },
-			{ nome: 'Corisco', preco: 67000 },
-			{ nome: 'Corsa', preco: 250000 },
-			{ nome: 'Corsa Sedan', preco: 420000 },
-			{ nome: 'Corsa Pick-up', preco: 490000 },
-			{ nome: 'Corsa Wagon', preco: 60000 }
-		]
+	constructor(
+		public navCtrl: NavController, 
+		private _http: Http,
+		private _loadingCtrl: LoadingController,
+		private _alertCtrl: AlertController
+	) { }
+	
+	ngOnInit() {
+		let loader = this._loadingCtrl.create({
+			content: 'Buscando Carros. Aguarde...'
+		});
+	
+		loader.present();
+	
+		this._http
+			.get('https://aluracar.herokuapp.com/')
+			.map(res => res.json())
+			.toPromise()
+			.then(carros => {
+				this.carros = carros;
+				loader.dismiss();
+			})
+			.catch(error => {
+				loader.dismiss();
+				this._alertCtrl.create({
+					title: 'Falha na conexão!',
+					subTitle: 'Não foi possível obter a lista de carros!',
+					buttons: [{
+						text: 'Estou ciente!'
+					}]
+				}).present()
+			});
+
 	}
 
 }
